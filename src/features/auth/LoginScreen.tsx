1561c90cf447aa7355e2b983c50e03cf0bfff1f1
Mohammad Aakash <muhammadaakash249@gmail.com>
2024-11-15 23:27:57 +0500

    Temp commit

--- src/features/auth/LoginScreen.tsx
+++ src/features/auth/LoginScreen.tsx
@@ -1,7 +1,8 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
   return (
     <View>
       <Text>LoginScreen</Text>
